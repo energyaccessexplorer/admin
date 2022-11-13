fbbc3086cd1dc7c067a5e8a9532d690d7471948a
Fabian <f@energyaccessexplorer.org>
2022-11-13 09:33:25 +0100

    master -> director

--- src/circles.js
+++ src/circles.js
@@ -1,6 +1,6 @@
 export function circles_user() {
 	const claims = jwt_decode(localStorage.getItem('token'));
 
-	if (['master', 'root'].includes(claims.role)) return null;
+	if (['director', 'root'].includes(claims.role)) return null;
 	else return claims['data']['circles'];
 };
